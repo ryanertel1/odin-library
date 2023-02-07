2cec4814546779b4d0bb7d9f95fa49a4c8f88dfa
Ryan Ertel <ertelryan5@gmail.com>
2023-02-06 22:20:05 -0500

    Implement form into modal

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const booksContainer = document.querySelector('.books-container');
 const myLibrary = [];
 const bookInput = document.querySelector('.book-input');
 
+// Dom objects for book entry modal
+const entryModal = document.querySelector('.entry-modal');
+const addBtn = document.querySelector('.add-button');
+const entryModalClose = document.getElementsByClassName('closeEntry')[0];
+// Dom objects for book information modal
+const bookModal = document.querySelector('.book-modal')
+const bookModalClose = document.getElementsByClassName('closeBook')[0];
+
 function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
@@ -58,24 +66,32 @@ bookInput.addEventListener('submit', (event) => {
     const inputStatus = document.getElementById('status').checked;
     addBook(inputTitle, inputAuthor, inputPages, inputStatus)
     bookInput.reset();
+    entryModal.style.display = 'none';
 });
 
-
-
-
-
-const modal = document.querySelector('.entry-modal');
-const btn = document.querySelector('.add-button');
-const span = document.getElementsByClassName('close')[0];
-
-btn.onclick = function() {
-    modal.style.display = 'block';
+addBtn.onclick = function() {
+    entryModal.style.display = 'block';
 }
-span.onclick = function() {
-    modal.style.display = 'none';
+entryModalClose.onclick = function() {
+    entryModal.style.display = 'none';
 }
 window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
+    if (event.target === entryModal) {
+        entryModal.style.display = 'none';
+    } else if(event.target === bookModal) {
+        bookModal.style.display = 'none';
     }
 }
+bookModalClose.onclick = function() {
+    bookModal.style.display = 'none';
+}
+
+booksContainer.addEventListener('click', (event) => {
+    const currentStatus = myLibrary[event.target.dataset.indexNumber].status;
+    if (currentStatus) {
+        myLibrary[event.target.dataset.indexNumber].status = false;
+    } else {
+        myLibrary[event.target.dataset.indexNumber].status = true;
+    }
+    generateTable();
+});
